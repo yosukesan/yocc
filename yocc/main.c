1cf2dc7816afe4fac9e339b06b2344d1353cf699
yosukesan <y.otsuki30@gmail.com>
2019-08-02 22:51:09 +0900

    add simple addition and substration.

---
+++ yocc/main.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2){
+        fprintf(stderr, "usage: ./a.out ${num}");
+        return 1;
+    }
+
+    char *p = argv[1];
+
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p){
+        if (*p == '+')
+        {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+    }
+    printf("    ret\n");
+
+    return 0;
+}
---
+++ yocc/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+try() {
+        expected="$1"
+        input="$2"
+
+        ./yocc.out "$input" > tmp.s
+        gcc -o tmp.out tmp.s
+        ./tmp.out
+        actual="$?"
+
+        if [ "$actual" = "$expected" ]; then
+            echo "$input => $actual"
+        else
+            echo "$input => $expected expected, but got $actual"
+            exit 1
+        fi
+}
+
+try 0 0
+try 42 42
+try 21 "5+20-4"
+
+echo OK
